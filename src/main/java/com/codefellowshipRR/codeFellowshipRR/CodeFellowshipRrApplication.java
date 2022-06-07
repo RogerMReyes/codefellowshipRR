663bf1120101d0b6e061805dec9eaca8238f1aa8
RogerMReyes <Rogerrmr14@gmail.com>
2022-06-07 13:38:44 -0700

    Base Files

---
+++ src/main/java/com/codefellowshipRR/codeFellowshipRR/CodeFellowshipRrApplication.java
@@ -0,0 +1,13 @@
+package com.codefellowshipRR.codeFellowshipRR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFellowshipRrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFellowshipRrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codefellowshipRR/codeFellowshipRR/CodeFellowshipRrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefellowshipRR.codeFellowshipRR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFellowshipRrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
